ec71d96e287bc26c9072ccb56ac2b37d719a9048
ana paula <ana.pmonteiro@mercadolivre.com>
2022-11-08 19:11:47 -0300

    feature/product: create model productm and repository

--- src/main/java/com/example/projetointegrador/model/Product.java
+++ src/main/java/com/example/projetointegrador/model/Product.java
@@ -1,4 +1,23 @@
 package com.example.projetointegrador.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 200, nullable = false)
+     private Double price;
+
+    @Column(length = 200, nullable = false)
+     private Double volume;
+
 }
--- src/main/java/com/example/projetointegrador/repository/IProductRepository.java
+++ src/main/java/com/example/projetointegrador/repository/IProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.projetointegrador.repository;
 
-public class IProductRepository {
+import com.example.projetointegrador.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductRepository extends JpaRepository<Product, Long> {
 }
