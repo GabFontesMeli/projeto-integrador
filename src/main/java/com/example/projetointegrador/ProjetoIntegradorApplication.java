a7034bc9014f16540c23d8f109375513fd1305b1
GabFontesMeli <gabriel.fontes@mercadolivre.com>
2022-11-08 16:23:42 -0300

    inital commit

--- src/main/java/com/example/projetointegrador/ProjetoIntegradorApplication.java
+++ src/main/java/com/example/projetointegrador/ProjetoIntegradorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoIntegradorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoIntegradorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoIntegradorApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/projetointegrador/ProjetoIntegradorApplicationTests.java
+++ src/test/java/com/example/projetointegrador/ProjetoIntegradorApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjetoIntegradorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
