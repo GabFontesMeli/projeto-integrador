25ebca5cd2c3576b3a052134eba0d59d96ff4256
mayumeli <mayu.satori@mercadolivre.com>
2022-11-08 21:16:50 -0300

    docs: add todo comments

--- src/main/java/com/example/projetointegrador/Controller/ProductController.java
+++ src/main/java/com/example/projetointegrador/Controller/ProductController.java
@@ -1,4 +1,5 @@
 // package com.example.projetointegrador.Controller;
 
 // public class ProductController {
+    // TODO: create product route
 // }
--- src/main/java/com/example/projetointegrador/service/InventoryService.java
+++ src/main/java/com/example/projetointegrador/service/InventoryService.java
@@ -4,5 +4,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class InventoryService {
-    
+    // TODO: service to create and update inventory
 }
