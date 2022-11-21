7274dc07b8ff349ffc7f3de0c1713d927b9e83f6
franklinlopes <gabriel.franklin@mercadolivre.com>
2022-11-21 19:58:04 -0300

    test: service cart item discount

--- src/test/java/com/example/projetointegrador/service/CartItemServiceTest.java
+++ src/test/java/com/example/projetointegrador/service/CartItemServiceTest.java
@@ -1,10 +1,14 @@
 package com.example.projetointegrador.service;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -17,9 +21,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.example.projetointegrador.exceptions.CartItemNotFoundException;
 import com.example.projetointegrador.model.CartItem;
 import com.example.projetointegrador.repository.CartItemRepository;
+import com.example.projetointegrador.setup.BaseTest;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
-public class CartItemServiceTest {
+public class CartItemServiceTest extends BaseTest {
     @InjectMocks
     private CartItemService cartItemService;
 
@@ -33,5 +41,21 @@ public class CartItemServiceTest {
         BDDMockito.given(cartItemRepository.findAllCartItemsByBatchProductExpirationDateLessThanEqual(any(LocalDate.class))).willReturn(cartItemsToExpire);
 
         assertThrows(CartItemNotFoundException.class, () -> cartItemService.discountOnCartItems(2.00));
+    }
+
+    @Test
+    void discountOnCartItems() throws CartItemNotFoundException, StreamReadException, DatabindException, IOException {
+        
+        ObjectMapper mp = new ObjectMapper();
+        mp.findAndRegisterModules();
+        
+        List<CartItem> cartItemToExpire = Arrays.asList(mp.readValue(new File(path + "/responsesBody/CartItem/getCartItemsResponse.json"), CartItem[].class));
+
+        List<CartItem> cartItem = new ArrayList<>();
+        cartItem.add(cartItemToExpire.get(1));
+
+        BDDMockito.given(cartItemRepository.findAllCartItemsByBatchProductExpirationDateLessThanEqual(any(LocalDate.class))).willReturn(cartItem);
+
+        assertAll("Discount on products in CartItem",  () -> cartItemService.discountOnCartItems(2.00));
     }
 }
